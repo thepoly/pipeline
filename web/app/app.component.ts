0a84b3d0fcb11eb75757517e57eac2cee4bff6a4
Rex Hu <rex.hu555@gmail.com>
2016-10-25 17:00:48 -0400

    Show details for a single story

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -1,6 +1,24 @@
 import { Component } from '@angular/core';
+
+export class Story {
+  id: number;
+  title: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>tacos</h1>'
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{story.title}} details!</h2>
+  <div><label>id: </label>{{story.id}}</div>
+  <div><label>title: </label>{{story.title}}</div>
+  `
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Pipeline';
+  story: Story = {
+  id: 1,
+  title: 'Rex is a newb'
+  };
+}

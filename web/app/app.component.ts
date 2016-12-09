238cebbb56b1a0549363b17b7f89e7217d5e5c7a
Sidney Kochman <sidney@kochman.org>
2016-12-09 11:00:51 -0500

    Restyle header

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -4,14 +4,16 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'my-app',
   template: `
-    <h1>
+    <div class="header">
       <img src="https://poly.rpi.edu/wp-content/themes/poly_new_testbed/images/logo_m.png">
-    </h1>
+    </div>
+    <div class="container">
     <nav>
       <a routerLink="/story-editor" routerLinkActive="active">Story Editor</a>
       <a routerLink="/story-search" routerLinkActive="active">Story Search</a>
     </nav>
     <router-outlet></router-outlet>
+    </div>
   `,
   styleUrls: ['app.component.css'],
 })

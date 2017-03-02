bd78874ec267721d7c7f4488e45d35930be2a836
Josh Berman <bermaj@rpi.edu>
2017-03-02 11:51:52 -0500

    Now will identify events for the next day and send a notification to the slack channel in settings.

--- api/slack.py
+++ api/slack.py
@@ -1,5 +1,9 @@
-import settings
 import requests
+import time
+import datetime
+
+import models
+import settings
 
 
 class SlackConnection:
@@ -14,8 +18,20 @@ class SlackConnection:
         data = {'token' : self.slack_settings.slack_api_key}
         r = requests.post(url, data=data)
         
-    def post(self, message):
+    def post(self, title, time):
         url = self.baseurl + "chat.postMessage"
-        data = { 'token' : self.slack_settings.slack_api_key, 'text' : message, 'channel' : self.slack_settings.slack_notification_channel, 'username' : "Pipeline Bot"}
+        data = { 'token' : self.slack_settings.slack_api_key, 'text' : "{} taking place at {} tomorrow".format(title, time), 'channel' : self.slack_settings.slack_notification_channel, 'username' : "Pipeline Bot"}
         r = requests.post(url, data=data)
 
+
+def get_stories():
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    events_tomorrow = models.Story.select().where(models.Story.event_time.day == tomorrow.day)
+    return events_tomorrow
+
+
+if __name__ == "__main__":
+    events = get_stories()
+    slack = SlackConnection()
+    for i in events:
+        slack.post(i.title, i.event_time)

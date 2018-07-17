e16a47b0a3c5b4eee880290dad118108d28efcb7
Sidney Kochman <sidney@kochman.org>
2018-07-17 12:25:27 -0400

    Add featured article to home page; add article summary

--- home/models.py
+++ home/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import PageChooserPanel
 
 
 class HomePage(Page):
-    pass
+    featured_article = models.ForeignKey(
+        'articles.ArticlePage',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        help_text='Article that is displayed prominently at the top of the home page.'
+    )
+
+    content_panels = Page.content_panels + [
+        PageChooserPanel('featured_article'),
+    ]

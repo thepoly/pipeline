6e07d4f8207323f0d0231bbba9a8702aa2f76687
Sidney Kochman <sidney@kochman.org>
2018-07-17 12:25:27 -0400

    Add featured article to home page; add article summary

--- articles/models.py
+++ articles/models.py
@@ -3,21 +3,36 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.blocks import RichTextBlock
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel
 
 from bs4 import BeautifulSoup
 
 
 class ArticlePage(Page):
     headline = RichTextField(features=['italic'])
+    subdeck = RichTextField(
+        features=['italic'],
+        null=True,
+        blank=True
+    )
     date = models.DateField()
     body = StreamField([
         ('paragraph', RichTextBlock()),
     ])
+    summary = RichTextField(
+        features=['italic'],
+        null=True,
+        blank=True,
+        help_text='Displayed on the home page or other places to provide a taste of what the article is about.'
+    )
 
     content_panels = [
-        FieldPanel('headline'),
+        MultiFieldPanel([
+            FieldPanel('headline'),
+            FieldPanel('subdeck'),
+        ]),
         FieldPanel('date'),
+        FieldPanel('summary'),
         StreamFieldPanel('body'),
     ]
 

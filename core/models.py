2e01b490eb8c6b805400be0d8fd5325ee1406f56
Sidney Kochman <sidney@kochman.org>
2018-08-17 19:27:20 -0400

    Add static page type

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
-# Create your models here.
+
+class StaticPage(Page):
+    body = RichTextField()
+
+    content_panels = Page.content_panels + [FieldPanel("body")]

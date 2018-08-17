b88be1910aa316cecc03d7608165c6c450e408e9
Sidney Kochman <sidney@kochman.org>
2018-08-17 15:46:04 -0400

    Allow blank kickers on articles

--- articles/models.py
+++ articles/models.py
@@ -3,7 +3,13 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.blocks import RichTextBlock
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel, InlinePanel, PageChooserPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    StreamFieldPanel,
+    MultiFieldPanel,
+    InlinePanel,
+    PageChooserPanel,
+)
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.images.blocks import ImageChooserBlock
@@ -15,75 +21,68 @@ from bs4 import BeautifulSoup
 
 
 class ArticlePage(Page):
-    headline = RichTextField(features=['italic'])
-    subdeck = RichTextField(
-        features=['italic'],
-        null=True,
-        blank=True
-    )
+    headline = RichTextField(features=["italic"])
+    subdeck = RichTextField(features=["italic"], null=True, blank=True)
     kicker = models.ForeignKey(
-        'articles.Kicker',
-        null=True,
-        on_delete=models.PROTECT)
+        "articles.Kicker", null=True, blank=True, on_delete=models.PROTECT
+    )
     date = models.DateField()
-    body = StreamField([
-        ('paragraph', RichTextBlock()),
-        ('image', ImageChooserBlock()),
-    ])
+    body = StreamField([("paragraph", RichTextBlock()), ("image", ImageChooserBlock())])
     summary = RichTextField(
-        features=['italic'],
+        features=["italic"],
         null=True,
         blank=True,
-        help_text='Displayed on the home page or other places to provide a taste of what the article is about.'
+        help_text="Displayed on the home page or other places to provide a taste of what the article is about.",
     )
     featured_photo = models.ForeignKey(
-        'wagtailimages.Image',
+        "wagtailimages.Image",
         null=True,
         blank=True,
         on_delete=models.PROTECT,
-        help_text='Shown at the top of the article and on the home page.'
+        help_text="Shown at the top of the article and on the home page.",
     )
 
     content_panels = [
-        MultiFieldPanel([
-                FieldPanel('headline', classname="title"),
-                FieldPanel('subdeck'),
-            ]),
-        MultiFieldPanel([
-                FieldPanel('date'),
+        MultiFieldPanel(
+            [FieldPanel("headline", classname="title"), FieldPanel("subdeck")]
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("date"),
                 # TODO: use https://github.com/wagtail/wagtail-autocomplete for kicker
-                SnippetChooserPanel('kicker'),
-                InlinePanel('article_author_relationship', label='Author', min_num=1),
-                ImageChooserPanel('featured_photo'),
+                SnippetChooserPanel("kicker"),
+                InlinePanel("article_author_relationship", label="Author", min_num=1),
+                ImageChooserPanel("featured_photo"),
             ],
-            heading='Metadata',
-            classname='collapsible'),
-        FieldPanel('summary'),
-        StreamFieldPanel('body'),
+            heading="Metadata",
+            classname="collapsible",
+        ),
+        FieldPanel("summary"),
+        StreamFieldPanel("body"),
     ]
 
     search_fields = Page.search_fields + [
-        index.SearchField('headline'),
-        index.SearchField('subdeck'),
-        index.SearchField('body'),
-        index.SearchField('summary'),
+        index.SearchField("headline"),
+        index.SearchField("subdeck"),
+        index.SearchField("body"),
+        index.SearchField("summary"),
     ]
 
     def clean(self):
         super().clean()
 
-        soup = BeautifulSoup(self.headline, 'html.parser')
+        soup = BeautifulSoup(self.headline, "html.parser")
         self.title = soup.text
-    
+
     def authors(self):
         return [r.author for r in self.article_author_relationship.all()]
 
 
 class ArticlesIndexPage(Page):
-    subpage_types = ['ArticlePage']
+    subpage_types = ["ArticlePage"]
 
     def get_articles(self):
-        return ArticlePage.objects.live().descendant_of(self).order_by('date')
+        return ArticlePage.objects.live().descendant_of(self).order_by("date")
 
 
 @register_snippet
@@ -95,15 +94,16 @@ class Kicker(models.Model):
 
 
 class ArticleAuthorRelationship(models.Model):
-    article = ParentalKey('ArticlePage', related_name='article_author_relationship', on_delete=models.PROTECT)
-    author = models.ForeignKey('authors.AuthorPage', related_name='author_article_relationship', on_delete=models.PROTECT)
-    title = RichTextField(
-        features=['italic'],
-        null=True,
-        blank=True
+    article = ParentalKey(
+        "ArticlePage",
+        related_name="article_author_relationship",
+        on_delete=models.PROTECT,
     )
+    author = models.ForeignKey(
+        "authors.AuthorPage",
+        related_name="author_article_relationship",
+        on_delete=models.PROTECT,
+    )
+    title = RichTextField(features=["italic"], null=True, blank=True)
 
-    panels = [
-        PageChooserPanel('author'),
-        FieldPanel('title'),
-    ]
+    panels = [PageChooserPanel("author"), FieldPanel("title")]

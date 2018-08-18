8ef6edc8993a77fdd03e0dbae7aae4feff241ab1
Sidney Kochman <sidney@kochman.org>
2018-08-18 16:24:40 -0400

    Fix n+1 queries in staff

--- pipeline/settings/dev.py
+++ pipeline/settings/dev.py
@@ -1,23 +1,34 @@
+import logging
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9v281(zz#j15vjq5yvnp(zvn=^=e)h@dfe-uj58#9p9+w&o*tm'
+SECRET_KEY = "9v281(zz#j15vjq5yvnp(zvn=^=e)h@dfe-uj58#9p9+w&o*tm"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "nplusone.ext.django.NPlusOneMiddleware",
 ] + MIDDLEWARE
 
-INSTALLED_APPS = INSTALLED_APPS + [
-    'debug_toolbar',
-]
+INSTALLED_APPS = INSTALLED_APPS + ["debug_toolbar", "nplusone.ext.django"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {"nplusone": {"handlers": ["console"], "level": "WARN"}},
+}
+
+NPLUSONE_LOGGER = logging.getLogger("nplusone")
+NPLUSONE_LOG_LEVEL = logging.WARN
 
 try:
     from .local import *

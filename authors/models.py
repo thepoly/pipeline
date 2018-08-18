fafb311f78d3450a9cdb37fa8c82149927be6481
Sidney Kochman <sidney@kochman.org>
2018-08-18 14:42:33 -0400

    Remove authors app

--- authors/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-from wagtail.core.fields import RichTextField
-from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
-from wagtail.search import index

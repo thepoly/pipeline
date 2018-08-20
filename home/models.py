74a94a82524f6e029f8fad2eed308b8ab353448c
Sidney Kochman <sidney@kochman.org>
2018-08-20 12:23:31 -0400

    Add recent articles homepage block

--- home/models.py
+++ home/models.py
@@ -5,13 +5,16 @@ from wagtail.core.fields import StreamField
 from wagtail.admin.edit_handlers import PageChooserPanel, StreamFieldPanel
 from wagtail.core import blocks
 
+from articles.models import ArticlePage
+
 
 class ArticleBlock(blocks.StructBlock):
     article = blocks.PageChooserBlock(target_model="articles.ArticlePage")
     headline = blocks.RichTextBlock(
-        help_text="Optional text to override the article headline", required=False
+        help_text="Optional. Will override the article's headline.", required=False
     )
     # TODO: add a photo override block
+    # TODO: add a "hide photo" block
 
     class Meta:
         template = "home/article_block.html"
@@ -19,6 +22,9 @@ class ArticleBlock(blocks.StructBlock):
 
 class OneColumnBlock(blocks.StructBlock):
     column = ArticleBlock()
+
+    def article_pks(self):
+        return set(self.column.value.pk)
 
 
 class TwoColumnBlock(blocks.StructBlock):
@@ -30,15 +36,47 @@ class TwoColumnBlock(blocks.StructBlock):
         help_text="Which article, if either, should appear larger.",
     )
 
+    def article_pks(self):
+        return set(self.left_column.value.pk, self.right_column.value.pk)
+
 
 class ThreeColumnBlock(blocks.StructBlock):
     left_column = ArticleBlock()
     middle_column = ArticleBlock()
     right_column = ArticleBlock()
 
+    def article_pks(self):
+        return set(
+            self.left_column.value.pk,
+            self.middle_column.value.pk,
+            self.right_column.value.pk,
+        )
+
+
+class RecentArticlesValue(blocks.StructValue):
+    def get_articles(self):
+        # Get the primary keys of all the articles manually placed on the homepage
+        # so that we can exclude them here.
+        homepage = HomePage.objects.get()
+        pks = homepage.article_pks()
+        return (
+            a
+            for a in ArticlePage.objects.order_by("-date").exclude(pk__in=pks)[
+                : self["num_articles"]
+            ]
+        )
+
 
 class RecentArticlesBlock(blocks.StructBlock):
-    num_articles = blocks.IntegerBlock()
+    num_articles = blocks.IntegerBlock(
+        help_text="Number of recent articles to display.", label="Number of articles"
+    )
+
+    class Meta:
+        value_class = RecentArticlesValue
+        admin_text = (
+            "Shows recent articles that are not manually placed elsewhere on the page."
+        )
 
 
 class HomePage(Page):
@@ -53,3 +91,17 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [StreamFieldPanel("featured_articles")]
+
+    def article_pks(self):
+        pks = set()
+        for block in self.featured_articles:
+            if block.block_type == "one_column":
+                pks.add(block.value["column"]["article"].pk)
+            elif block.block_type == "two_columns":
+                pks.add(block.value["left_column"]["article"].pk)
+                pks.add(block.value["right_column"]["article"].pk)
+            elif block.block_type == "three_columns":
+                pks.add(block.value["left_column"]["article"].pk)
+                pks.add(block.value["middle_column"]["article"].pk)
+                pks.add(block.value["right_column"]["article"].pk)
+        return pks

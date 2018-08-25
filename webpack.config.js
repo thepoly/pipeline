1a94a366fbcb60c14c812554d400b99d2f52efbc
Sidney Kochman <sidney@kochman.org>
2018-08-25 17:16:04 -0400

    Fix dev webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'none',

53e3d93d81e8c8ced5870edafda8898c9492678d
Sidney Kochman <sidney@kochman.org>
2018-08-27 16:33:41 -0400

    Use Postgres instead of SQLite

--- pipeline/settings/base.py
+++ pipeline/settings/base.py
@@ -91,8 +91,10 @@ WSGI_APPLICATION = "pipeline.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pipeline",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 

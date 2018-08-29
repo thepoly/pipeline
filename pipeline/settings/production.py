3dc153f84379f007dcb8f3557107d1bd0cb95621
Sidney Kochman <sidney@kochman.org>
2018-08-29 17:02:01 -0400

    Use Postgres with Docker Compose

--- pipeline/settings/production.py
+++ pipeline/settings/production.py
@@ -11,6 +11,17 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "poly.rpi.edu"]
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pipeline",
+        "HOST": "postgres",
+        "PORT": "5432",
+        "USER": "pipeline",
+        "PASSWORD": "pipeline",
+    }
+}
+
 try:
     from .local import *
 except ImportError:

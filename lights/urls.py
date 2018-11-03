f863d7a3507f4d3908a5e4b07021846ef309eb77
Sidney Kochman <sidney@kochman.org>
2018-11-03 16:29:20 -0400

    fix lights URLs

--- lights/urls.py
+++ lights/urls.py
@@ -6,6 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("LEDP.txt", views.getColor, name="get color"),
-    path("admin/", admin.site.urls),
     path("submit", views.setColor, name="set color"),
 ]

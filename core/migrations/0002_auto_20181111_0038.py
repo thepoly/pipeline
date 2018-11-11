4f1503e039fa50c2bf4c6f0ee5c7d45fc29c9619
Sidney Kochman <sidney@kochman.org>
2018-11-10 20:01:17 -0500

    black

--- core/migrations/0002_auto_20181111_0038.py
+++ core/migrations/0002_auto_20181111_0038.py
@@ -6,14 +6,17 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('core', '0001_initial'),
-    ]
+    dependencies = [("core", "0001_initial")]
 
     operations = [
         migrations.AlterField(
-            model_name='staffpage',
-            name='photo',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, to='core.CustomImage'),
-        ),
+            model_name="staffpage",
+            name="photo",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                to="core.CustomImage",
+            ),
+        )
     ]

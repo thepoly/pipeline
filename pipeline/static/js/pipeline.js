d4be0f458cc0f102aede8f1f67509e8a5885e22b
Sidney Kochman <sidney@kochman.org>
2019-04-17 11:44:49 -0400

    add google analytics tracking code; fire on turbolinks load

--- pipeline/static/js/pipeline.js
+++ pipeline/static/js/pipeline.js
@@ -6,5 +6,13 @@ Turbolinks.start();
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start();
-const context = require.context("./controllers", true, /\.(js|ts)$/)
-application.load(definitionsFromContext(context))
+const context = require.context("./controllers", true, /\.(js|ts)$/);
+application.load(definitionsFromContext(context));
+
+// Google Analytics
+document.addEventListener("turbolinks:load", (event) => {
+    if (typeof ga === "function") {
+        ga('set', 'location', event.data.url);
+        ga('send', 'pageview');
+    }
+});

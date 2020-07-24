01bcf5f1fbaca2d08d5ac06139504a9b392d7762
Namish <namishgali@gmail.com>
2020-07-23 22:07:07 -0400

    Revisions to Django staticfiles import
    See: https://docs.djangoproject.com/en/dev/internals/deprecation/
    For more info on Django deprecation.
    Attempting to establish compatibility with Django 3.0 and Wagtail 2.9.2

--- pipeline/settings/base.py
+++ pipeline/settings/base.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.templatetags.static",
     "django.contrib.sitemaps",
     "webpack_loader",
     "django_prometheus",
@@ -138,6 +139,7 @@ USE_TZ = True
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.templatetags.static",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
